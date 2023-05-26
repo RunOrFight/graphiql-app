c5787db03da4e8cb0ae27da9081b03bc7ffaf4bc
RunOrFight <dragoinfury@gmail.com>
2023-05-26 13:01:01 +0300

    feat: add basic routes

---
+++ src/layout/Root.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
+import Header from './Header';
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Root;
